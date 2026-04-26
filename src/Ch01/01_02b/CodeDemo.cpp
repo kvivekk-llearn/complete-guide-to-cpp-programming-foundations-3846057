66279014a7a26bf3137f594777d1043ced762e69
kvivekk-llearn <krishna.vivekk27@gmail.com>
2026-04-26 01:50:05 +0000

    first chapter finished code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,30 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+/* this is a simple C++ program
+ from the Linked in Learning*/
+
+ int add (int num1, int num2){
+    int result;
+
+    result = num1 + num2;
+
+    return result;
+ }
+ 
+ 
+ 
+ int main () {
+
+    std::cout << " Hi There" <<std::endl << "The add function" << std::endl ;
+
+    int result = add(3.5, 5.5);
+
+    std::cout << "The result is =" << result << std::endl;
+
+    std::cout << std::endl <<std::endl;
+
+    return 0; // always return from function
+ }
