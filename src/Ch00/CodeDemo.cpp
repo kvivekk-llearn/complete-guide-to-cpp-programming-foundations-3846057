435c870dc28b31a644681617588f4c5e6fcca835
kvivekk-llearn <krishna.vivekk27@gmail.com>
2026-04-26 01:13:56 +0000

    Added a new comment to test the commit

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03; my first commit
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
