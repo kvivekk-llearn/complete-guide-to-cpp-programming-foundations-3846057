54cfbb87f6bfe7704681b2062ee9b2984d9b55be
kvivekk-llearn <krishna.vivekk27@gmail.com>
2026-04-27 23:27:48 +0000

    added code to this file

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,31 @@
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
     
+    a= 7;
+    bool my_flag = false;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+
+    my_flag=true;
+
+    std::cout << "a+b = " << a+b << std::endl;
+    std::cout << "b-a = " << b-a << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+
+    unsigned positive;
+
+    positive = b-a;
+
+    std::cout << "unsigned b-a = " << positive << std::endl;
+
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
